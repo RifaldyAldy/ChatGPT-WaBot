893b78db083aba3747f93d6823daad4f31aea529
RifaldyAldy <112386802+RifaldyAldy@users.noreply.github.com>
2023-06-23 21:32:30 +0700

    Update openai.js

--- openai.js
+++ openai.js
@@ -1,78 +1,34 @@
-// Menambahkan dependencies
-const { default: makeWASocket, DisconnectReason, useMultiFileAuthState } = require('@whiskeysockets/baileys');
-const Boom = require('@hapi/boom');
-const GPT = require('./openai');
+const { Configuration, OpenAIApi } = require('openai');
 
-// fungsi utama WA BOT
-async function connectToWhatsapp() {
-  const { state, saveCreds } = await useMultiFileAuthState('Auth_info_logins');
-  // console.log(state);
-
-  // buat koneksi baru
-  const sock = makeWASocket({
-    auth: state,
-    printQRInTerminal: true,
-    defaultQueryTimeoutMs: undefined,
-  });
-
-  // Handler untuk event 'connection.update'
-  function handleConnectionUpdate(update) {
-    const { connection, lastDisconnect } = update;
-    if (connection === 'close') {
-      const reconnecting = (lastDisconnect.error === Boom)?.output?.statusCode !== DisconnectReason.loggedOut;
-      console.log(`Koneksi terputus karena ${lastDisconnect.error}, Hubungkan kembali! ${reconnecting}`);
-      if (reconnecting) {
-        connectToWhatsapp();
-      }
-    } else if (connection === 'open') {
-      console.log('Terhubung!');
-    }
-  }
-
-  // Menambahkan listener untuk event 'connection.update'
-  sock.ev.on('connection.update', handleConnectionUpdate);
-
-  await sock.ev.on('creds.update', saveCreds);
-
-  // Fungsi untuk memantau pesan masuk
-  sock.ev.on('messages.upsert', async ({ messages, type }) => {
-    console.log(`Tipe pesan : ${type}`);
-    if (type === 'notify' && !messages[0].key.fromMe) {
-      try {
-        console.log(messages);
-        const number = messages[0].key.remoteJid;
-        let chat = messages[0].message.conversation;
-        if (!chat) {
-          chat = messages[0].message.extendedTextMessage.text;
-        }
-        const ambilRequestChat = chat.split(' ').slice(1).join(' ').toString();
-        const ambilRequest = ambilRequestChat.split(' ')[0];
-
-        //membuat variabel untuk mengecek apakah pesan dari group dan mention bot
-        const isMessageGroup = number.includes('@g.us');
-        const isMessageMentionBot = chat.includes('@628388995241');
-
-        console.log(number);
-        console.log(chat);
-        if (chat.toLowerCase() === 'ping') {
-          const send = await sock.sendMessage(number, { text: 'Halo, selamat datang di AlfaMampus!' }, { quoted: messages[0] }, 2000);
-        }
-
-        //logic jika mention bot saja
-        if (isMessageGroup && isMessageMentionBot && ambilRequest === '/nanya') {
-          await sock.sendMessage(number, { text: 'Sebentar sedang mikir...' }, { quoted: messages[0] }, 2000);
-          const req = await GPT(ambilRequestChat);
-          await sock.sendMessage(number, { text: req }, { quoted: messages[0] }, 2000);
-          console.log(ambilRequestChat);
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  });
-}
-
-// menjalankan Whatsapp bot
-connectToWhatsapp().catch((error) => {
-  console.log(error);
+const key = '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';
+// const key = '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';
+const configuration = new Configuration({
+  apiKey: key,
+  baseURL: 'https://api.openai.com/v1/chat/completions',
 });
+const openai = new OpenAIApi(configuration);
+
+const GPT = async (chat) => {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${chat}`,
+    temperature: 0,
+    max_tokens: 1000,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+
+  // return response.data.choices[0].text;
+  return response.data.choices[0].text || 'saya tidak tau!';
+};
+
+GPT()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+module.exports = GPT;
